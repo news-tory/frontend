33c3e899611dccfab639271e5b58c59866f792a7
Seoheeda <1015aliciay@gmail.com>
2023-07-24 20:17:55 +0900

    change

---
+++ src/pages/main/index.jsx
@@ -0,0 +1,16 @@
+import Header from '../../components/header';
+import Sidebar from '../../components/sidebar';
+import Body from './body';
+
+function Main() {
+    return (
+        <>
+            <Header></Header>
+            <Sidebar></Sidebar>
+            <Body></Body>
+        </>
+
+    )
+}
+
+export default Main;
