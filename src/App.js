2de0be1a6ae05eb5acab3784ad8abbe602f58521
minhyung <dup33333@hufs.ac.kr>
2023-07-24 20:17:16 +0900

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react'
+import {BrouseRouter, Routes, Route, BrowserRouter} from 'react-router-dom'
+import Main from "./pages/main"
+import Community from "./pages/community"
+import Mypage from "./pages/Mypage"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main/>}></Route>
+          <Route path='/community/' element={<Community/>}></Route>
+          <Route path='/mypage/' element={<Mypage/>}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
